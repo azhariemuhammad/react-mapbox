9da03daecc44c2714f0bf95ec9959a3478c700ef
azhariemuhammad <azharie_muhammad@live.com>
2018-02-19 15:27:02 +0700

    data.json

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
+import ReactMapboxGl, { Layer, Feature, Popup } from 'react-mapbox-gl'
 
 // tslint:disable-next-line:no-var-requires
 import data from '../heatMapData.json'
 // tslint:disable-next-line:no-var-requires
 const token = `pk.eyJ1IjoiYXpoYXJpZSIsImEiOiJjamQ5cjVmYnMyYWs1MzNsYm5nNjY0cGg3In0.dUboQUB7N3Nhk2Ot1zBFMQ`
-console.log(token)
 
 const styles = {
   dark: 'mapbox://styles/mapbox/dark-v9'
@@ -17,78 +16,54 @@ const mapStyle = {
   flex: 1
 };
 
-// const Props =  {
-//   /* eslint-disable-next-line:no-any */
-//   onStyleLoad: (map: any) => any
-// }
 
 const layerPaint = {
   'heatmap-weight': {
-    property: 'priceIndicator',
+    property: 'API',
     type: 'exponential',
     stops: [
       [0, 0],
       [5, 2]
     ]
   },
-  // Increase the heatmap color weight weight by zoom level
-  // heatmap-ntensity is a multiplier on top of heatmap-weight
-  'heatmap-intensity': {
-    stops: [
-      [0, 0],
-      [5, 1.2]
-    ]
-  },
-  // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-  // Begin color ramp at 0-stop with a 0-transparancy color
-  // to create a blur-like effect.
-  // 'heatmap-color': {
-  //   stops: [
-  //     [0, 'rgba(33,102,172,0)'],
-  //     [0.25, 'rgb(103,169,207)'],
-  //     [0.5, 'rgb(209,229,240)'],
-  //     [0.8, 'rgb(253,219,199)'],
-  //     [1, 'rgb(239,138,98)'],
-  //     [2, 'rgb(178,24,43)']
-  //   ]
-  // }
-  // Adjust the heatmap radius by zoom level
-  // 'heatmap-radius': {
-  //   stops: [
-  //     [0, 1],
-  //     [5, 50]
-  //   ]
-  // }
+  'heatmap-radius': 40
 };
 
 
-const Heatmap = () => {
-  const center = [126.07014059, -3.2333765]
+class Heatmap extends Component {
+  constructor() {
+    super()
+    this.state = {
+      center: [126.07014059, -3.2333765]
+    } 
 
-  /* eslint-disable-next-line:no-any */
-  // const onStyleLoad = (map: any ) => {
-  //   const { onStyleLoad } = Props;
-  //   return onStyleLoad && onStyleLoad(map);
-  // };
-  console.log(layerPaint)
+  }
+  render () {
+    console.log(Feature)
     return (
-      
+      <div>
       <Map
-        center={center}
-        zoom={[6]}
-        style="mapbox://styles/mapbox/streets-v9"
+        center={this.state.center}
+        zoom={[5]}
+        style={styles.dark}
         containerStyle={{
           height: "100vh",
           width: "100vw"
         }}>
-        <Layer type="heatmap" paint={layerPaint}>
+          <Layer type="heatmap" paint={layerPaint}>
           {data.map((el, index) => (
-            <Feature key={index} coordinates={el.latlng} properties={el} />
+              <Feature 
+                key={index} 
+                coordinates={el.latlng} 
+                properties={el} 
+            />
           ))}
         </Layer>
       </Map>
+    </div>
       
     );
+  }
   
 }
 
